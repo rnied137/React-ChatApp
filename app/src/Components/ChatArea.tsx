cf780b81bb773d0e42201388c44c006d85515b3a
Szelma139 <raduniek1@gmail.com>
2021-05-04 16:13:14 +0200

    chat messages

--- app/src/Components/ChatArea.tsx
+++ app/src/Components/ChatArea.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import styled from 'styled-components';
 
+import { Message } from './Message';
 
 const Chat = styled.div`
 padding: ${props=>props.theme.padding.pad2};
 background-color:  ${props => props.theme.colors.white};
 min-height: 50px;
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+
 
 
 `;
 export const ChatArea = () => {
     return (
         <Chat>
-            asdas
+            <Message>Jutro pojde do sklepu</Message>
+
+            <Message isUser>Jutro pojde do na dziewczyny</Message>
+            <Message>Hello! Finally found the time to write to you) I need your help in creating interactive animations for my mobile application. Hello! Finally found the time to write to you) I need your help in creating interactive animations for my mobile application. </Message>
+
+
         </Chat>
     )
 }
